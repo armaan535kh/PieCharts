dc8be044085fd35015c9e9d95bf20222cd5deb5f
armaan535kh <kh.armaan535@gmail.com>
2024-09-14 06:33:44 +0530

    Initial Commit

---
+++ PieCharts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PieCharts
+//
+//  Created by Armaan Khan  on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PieCharts/PieChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartsApp.swift
+//  PieCharts
+//
+//  Created by Armaan Khan  on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PieChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
