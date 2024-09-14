062e61ec1351b6e733a501ccbd4a51bf6fc56377
armaan535kh <kh.armaan535@gmail.com>
2024-09-14 07:42:15 +0530

    Donut chart

--- PieCharts/ContentView.swift
+++ PieCharts/ContentView.swift
@@ -6,19 +6,47 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Chart {
+                    ForEach(MockData.revenueStreams) { stream in
+                        SectorMark(angle: .value("Stream", stream.value),
+                                   //innerRadius: .ratio(0.618),
+                                   outerRadius: stream.name == "Sponsors" ? 180:120,
+                                   angularInset: 1.0
+                        )
+                        .foregroundStyle(stream.color)
+                        .cornerRadius(6)
+                    }
+                }
+                .chartLegend(.hidden)
+            }
+            .padding()
+            .navigationTitle("Revenue")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct RevenueStream: Identifiable {
+    let id = UUID()
+    let name: String
+    let value: Double
+    let color: Color
+}
+
+struct MockData {
+    static var revenueStreams: [RevenueStream] = [
+        .init(name: "Adsense", value: 806, color: .teal),
+        .init(name: "Courses", value: 17855, color: .pink),
+        .init(name: "Sponsors", value: 4000, color: .indigo),
+        .init(name: "Consulting", value: 2500, color: .purple)
+        ]
 }
